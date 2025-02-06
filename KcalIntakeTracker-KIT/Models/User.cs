980999503520f750f516b8f88fe066cf6e4ed2fb
Kristjan Kristmundsson <kristjanbaldurk@gmail.com>
2025-02-06 16:13:12 +0100

    .

--- KcalIntakeTracker-KIT/Models/User.cs
+++ KcalIntakeTracker-KIT/Models/User.cs
@@ -6,6 +6,8 @@
         public string Username { get; set; } = string.Empty;
         public double Weight { get; set; }  // kg
         public double FatPercentage { get; set; }  // %
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
 
         // Navigation Property
         public List<DailyLog> DailyLogs { get; set; } = new();
